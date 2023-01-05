fe5b575f70e92bdf1f8ec4114d2387da999c8e37
Jamey Bryce <jybryce4@gmail.com>
2023-01-05 10:16:52 -0600

    config struct and parse_config function made

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,24 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for '{}'", query);
-    println!("in file {}", file_path);
+    println!("Searching for '{}'", config.query);
+    println!("in file '{}'", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Error reading file!");
+    let contents = fs::read_to_string(config.file_path).expect("Error reading file!");
 
     println!("with text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
