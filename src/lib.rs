8042562bdd869e1d7f615eba66c742c0219a2aba
Jamey Bryce <jybryce4@gmail.com>
2023-01-05 13:12:05 -0600

    modularized code with a lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use std::{
+    fs, 
+    error::Error
+};
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments. Expected 2 (search term & file).");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("with text:\n{contents}");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::{
     env, 
-    fs, 
-    process, 
-    error::Error
+    process
 };
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,30 +16,8 @@ fn main() {
     println!("Searching for '{}'", config.query);
     println!("in file '{}'", config.file_path);
 
-    run(config);
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments. Expected 2 (search term & file).");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("with text:\n{contents}");
-
-    Ok(())
 }
