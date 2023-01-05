836d4ada75b49b0b3574d18c90306cdec648828c
Jamey Bryce <jybryce4@gmail.com>
2023-01-05 12:44:39 -0600

    changed to handle error w/ Result

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments {err}");
+        process::exit(1);
+    });
 
     println!("Searching for '{}'", config.query);
     println!("in file '{}'", config.file_path);
@@ -19,17 +22,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!(
-                "Not enough arguments. Expected 2 but got {}",
-                args.len() - 1
-            );
+            return Err("Not enough arguments. Expected 2 (search term & file).");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
