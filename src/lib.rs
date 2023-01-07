187d3cd00395c0fe1854bc142a4d8ef1fc862388
Jamey Bryce <jybryce4@gmail.com>
2023-01-07 07:50:47 -0600

    used iterator for args instead

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,27 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments. Expected 2 (search term & file).");
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { 
-            query, 
+        Ok(Config {
+            query,
             file_path,
-            ignore_case
+            ignore_case,
         })
     }
 }
